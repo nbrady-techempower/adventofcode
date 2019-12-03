f9387defd23bbbd521ab7877798302e6f88b9a7b
Nate Brady <nathan.brady@yahoo.com>
2019-12-02 22:58:02 -0800

    2019 day 3

--- 2019/day3.js
+++ 2019/day3.js
@@ -1,8 +1,38 @@
-const input = require('./day1.input.js').split('\n');
+const input = require('./day3.input.js').split('\n').map(a => a.split(','));
 
-/** Part 1 **/
-let ans;
-console.log('Part 1: ', ans);
+const hash = [{}, {}];
 
-/** Part 2 **/
-console.log('Part 2: ', ans);
+function addToHash(x, y, hash, steps) {
+  const pnt = x + "," + y;
+  if (hash[pnt]) return;
+  hash[pnt] = steps;
+}
+
+input.forEach((inp, idx) => {
+  let x = 0, y = 0, steps = 0, h = hash[idx];
+  inp.forEach(p => {
+    const d = Math.abs(+p.replace(/[^\d]/g, ''));
+    for (let i = 0; i < d; i++) {
+      steps++;
+      if (p[0] === "R") x++;
+      if (p[0] === "U") y++;
+      if (p[0] === "D") y--;
+      if (p[0] === "L") x--;
+      addToHash(x, y, h, steps);
+    }
+  });
+});
+
+let min = Infinity, minSteps = Infinity;
+for (let h in hash[0]) {
+  if (hash[0][h] && hash[1][h]) {
+    const [x, y] = h.split(',');
+    const steps = hash[0][h] + hash[1][h];
+    const a = Math.abs(0 - +x) + Math.abs(0 - +y);
+    if (steps < minSteps) minSteps = steps;
+    if (a < min) min = a;
+  }
+}
+
+console.log('Part 1: ', min);
+console.log('Part 2: ', minSteps);
---
+++ 2019/day4.js
@@ -0,0 +1,8 @@
+const input = require('./day4.input.js').split('\n');
+
+/** Part 1 **/
+let ans;
+console.log('Part 1: ', ans);
+
+/** Part 2 **/
+console.log('Part 2: ', ans);
